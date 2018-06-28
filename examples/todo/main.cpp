03ffae1a5d5f8b1dd4d5747602ffc7d225b7e741
Ben Lau <xbenlau@gmail.com>
2018-06-29 00:48:57 +0800

    examples/todo: Register QML types manually

--- examples/todo/main.cpp
+++ examples/todo/main.cpp
@@ -1,12 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QFAppDispatcher>
+#include <QuickFlux>
 
 int main(int argc, char *argv[])
 {
     qputenv("QML_DISABLE_DISK_CACHE", "true");
 
     QGuiApplication app(argc, argv);
+
+    registerQuickFluxQmlTypes(); // It is not necessary to call this function if the QuickFlux library is installed via qpm
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
